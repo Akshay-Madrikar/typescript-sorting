8670cc32fa7d5d3785c27d18613404d2d74bab90
Akshay Madrikar <akshaymadrikar@gmail.com>
2020-11-20 19:36:38 +0530

    LinkedList sorted!

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
-import { Sorter } from './Sorter';
-import { NumbersCollection } from './NumbersCollection';
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
+import { CharacterCollection } from "./CharacterCollection";
+import { LinkedList } from "./LinkedList";
 
-const numbersCollection = new NumbersCollection([10,14,1,13]);
+const numbersCollection = new NumbersCollection([10, 14, 1, 13]);
+const characterCollection = new CharacterCollection("aSdEbHg");
+const linkedList = new LinkedList();
+linkedList.add(10);
+linkedList.add(-5);
+linkedList.add(4);
+linkedList.add(2);
 
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+const sortNumber = new Sorter(numbersCollection);
+const sortCharacter = new Sorter(characterCollection);
+const sortLinkedList = new Sorter(linkedList);
+sortNumber.sort();
+sortCharacter.sort();
+sortLinkedList.sort();
+
 console.log(numbersCollection.data);
+console.log(characterCollection.data);
+console.log(linkedList.print());
